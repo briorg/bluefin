40628790942917087b03660a9bfa3930d014c0a6
284789+b-@users.noreply.github.com <284789+b-@users.noreply.github.com>
2023-10-07 18:49:45 -0400

    just 1password

--- scripts/1password.sh
+++ scripts/1password.sh
@@ -11,52 +11,43 @@ mkdir /var/opt # temporary storage, will not end up in ostree
 rpm-ostree install https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
 
 mv /var/opt/1Password /usr/lib/1Password
-cat >/usr/bin/install-1password <<EOF
-#!/bin/bash
-#!/bin/sh
-set -eu
+ln -s /opt/1Password/1password /usr/bin/1password
 
 # hacked from 1password-latest.tar.gz//after-install.sh
 
-  # chrome-sandbox requires the setuid bit to be specifically set.
-  # See https://github.com/electron/electron/issues/17972
-  chmod 4755 ./chrome-sandbox
+# chrome-sandbox requires the setuid bit to be specifically set.
+# See https://github.com/electron/electron/issues/17972
+chmod 4755 ./chrome-sandbox
 
-  GROUP_NAME="onepassword"
+GROUP_NAME="onepassword"
+GID_OP="1500"
+GID_OPCLI="1600"
 
-  # Setup the Core App Integration helper binary with the correct permissions and group
-  if [ ! "$(getent group "${GROUP_NAME}")" ]; then
-    # GID must be > 1000, and I'd prefer GID > highest user GID
-    groupadd -K GID_MIN=1500 "${GROUP_NAME}"
-  fi
+# Setup the Core App Integration helper binary with the correct permissions and group
+# if [ ! "$(getent group "${GROUP_NAME}")" ]; then
+#   # GID must be > 1000, and I'd prefer GID > highest user GID
+#   groupadd -K GID_MIN=1500 "${GROUP_NAME}"
+# fi
 
-  HELPER_PATH="./1Password-KeyringHelper"
-  BROWSER_SUPPORT_PATH="./1Password-BrowserSupport"
+HELPER_PATH="./1Password-KeyringHelper"
+BROWSER_SUPPORT_PATH="./1Password-BrowserSupport"
 
-  chgrp "${GROUP_NAME}" $HELPER_PATH
-  # The binary requires setuid so it may interact with the Kernel keyring facilities
-  chmod u+s $HELPER_PATH
-  chmod g+s $HELPER_PATH
+chgrp "${GID_OP}" $HELPER_PATH
+# The binary requires setuid so it may interact with the Kernel keyring facilities
+chmod u+s $HELPER_PATH
+chmod g+s $HELPER_PATH
 
-  # This gives no extra permissions to the binary. It only hardens it against environmental tampering.
-  chgrp "${GROUP_NAME}" $BROWSER_SUPPORT_PATH
-  chmod g+s $BROWSER_SUPPORT_PATH
+# This gives no extra permissions to the binary. It only hardens it against environmental tampering.
+chgrp "${GROUP_NAME}" $BROWSER_SUPPORT_PATH
+chmod g+s $BROWSER_SUPPORT_PATH
 
-  # Restore previous directory
-  cd "$CWD"
+# Restore previous directory
+cd "$CWD"
 
-  # Register path symlink
+# Register path symlink
 ln -s /usr/lib/1Password /opt/1Password
 EOF
 chmod +x /usr/bin/install-1password
-
-# # Rewrite some hard set paths here
-# grep -rl "/opt/1Password" /usr/lib/1Password | xargs sed -i 's/\/opt\/1Password/\/usr\/lib\/1Password/g'
-# grep -rl "/opt/1Password" /usr/share/applications | xargs sed -i 's/\/opt\/1Password/\/usr\/lib\/1Password/g'
-#
-# # And redo the binary link
-# rm /usr/bin/1password
-# ln -s /usr/lib/1Password/1password /usr/bin/1password
 
 # Then we install the 1password CLI binary as well
 
@@ -66,7 +57,7 @@ unzip op_linux_amd64_v2.14.0.zip
 
 mv op /usr/bin
 
-groupadd -K GID_MIN=1500 onepassword-cli
+groupadd -g ${GID_OPCLI} onepassword-cli
 chown root:onepassword-cli /usr/bin/op
 chmod g+s /usr/bin/op
 
