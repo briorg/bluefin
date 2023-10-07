86f4c89967dac744de91e7f82f19695e6809dc58
284789+b-@users.noreply.github.com <284789+b-@users.noreply.github.com>
2023-10-07 16:37:25 -0400

    update 1password script

--- scripts/1password.sh
+++ scripts/1password.sh
@@ -3,22 +3,49 @@
 set -e
 
 echo "Installing 1Password"
-cd /usr/lib
-wget -qO- https://downloads.1password.com/linux/tar/stable/x86_64/1password-latest.tar.gz | tar -zxv
-ln -s 1Password 1password-*
-cd "$(mktemp -d)"
+#cd /usr/lib
+#wget -qO- https://downloads.1password.com/linux/tar/stable/x86_64/1password-latest.tar.gz | tar -zxv
+#ln -s 1Password 1password-*
 
-mkdir /var/opt
+mkdir /var/opt # temporary storage, will not end up in ostree
 rpm-ostree install https://downloads.1password.com/linux/rpm/stable/x86_64/1password-latest.rpm
 
-# rpm -ivh ./1password-latest.rpm
-
-# This is where the mess starts. 1Password is installed to /opt/1Password with
-# No way to change it. RIP. So we kinda _hack_ it and hope nothing is hard set
-# in the compiled code :(
 mv /var/opt/1Password /usr/lib/1Password
-cat > /usr/bin/install-1password <<EOF
+cat >/usr/bin/install-1password <<EOF
 #!/bin/bash
+#!/bin/sh
+set -eu
+
+# hacked from 1password-latest.tar.gz//after-install.sh
+
+  # chrome-sandbox requires the setuid bit to be specifically set.
+  # See https://github.com/electron/electron/issues/17972
+  chmod 4755 ./chrome-sandbox
+
+  GROUP_NAME="onepassword"
+
+  # Setup the Core App Integration helper binary with the correct permissions and group
+  if [ ! "$(getent group "${GROUP_NAME}")" ]; then
+    # GID must be > 1000, and I'd prefer GID > highest user GID
+    groupadd -K GID_MIN=1500 "${GROUP_NAME}"
+  fi
+
+  HELPER_PATH="./1Password-KeyringHelper"
+  BROWSER_SUPPORT_PATH="./1Password-BrowserSupport"
+
+  chgrp "${GROUP_NAME}" $HELPER_PATH
+  # The binary requires setuid so it may interact with the Kernel keyring facilities
+  chmod u+s $HELPER_PATH
+  chmod g+s $HELPER_PATH
+
+  # This gives no extra permissions to the binary. It only hardens it against environmental tampering.
+  chgrp "${GROUP_NAME}" $BROWSER_SUPPORT_PATH
+  chmod g+s $BROWSER_SUPPORT_PATH
+
+  # Restore previous directory
+  cd "$CWD"
+
+  # Register path symlink
 ln -s /usr/lib/1Password /opt/1Password
 EOF
 chmod +x /usr/bin/install-1password
@@ -33,13 +60,13 @@ chmod +x /usr/bin/install-1password
 
 # Then we install the 1password CLI binary as well
 
+cd "$(mktemp -d)"
 wget -q https://cache.agilebits.com/dist/1P/op2/pkg/v2.14.0/op_linux_amd64_v2.14.0.zip
-
 unzip op_linux_amd64_v2.14.0.zip
 
 mv op /usr/bin
 
-groupadd onepassword-cli
+groupadd -K GID_MIN=1500 onepassword-cli
 chown root:onepassword-cli /usr/bin/op
 chmod g+s /usr/bin/op
 
