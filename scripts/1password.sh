58d0d9093d1c51438379e06d4b9c234caff5af5e
284789+b-@users.noreply.github.com <284789+b-@users.noreply.github.com>
2023-10-07 23:24:25 -0400

    remove existing symlink

--- scripts/1password.sh
+++ scripts/1password.sh
@@ -13,6 +13,7 @@ rpm-ostree install https://downloads.1password.com/linux/rpm/stable/x86_64/1pass
 mv /var/opt/1Password /usr/lib/1Password
 
 #create symlink /usr/bin/1password pointing to /opt/1Password/1password
+rm /usr/bin/1password
 ln -s /opt/1Password/1password /usr/bin/1password
 
 # hacked from 1password-latest.tar.gz//after-install.sh
