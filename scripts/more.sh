4357dac8c5ee3c77ab087b01b4266fae3f75954c
bri <284789+b-@users.noreply.github.com>
2023-10-15 00:37:22 -0400

    fix: discord

--- scripts/more.sh
+++ scripts/more.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 wget 'https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage' -Lo /usr/bin/nvim.appimage
 #wget 'https://download.beeper.com/linux/appImage/x64' -Lo /usr/bin/beeper.appimage
@@ -7,3 +7,8 @@ wget 'https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage' 
 ## install discord
 curl -L 'https://discord.com/api/download?platform=linux&format=tar.gz' | tar -xzvC /usr/lib
 cp /usr/lib/Discord/discord.desktop /usr/share/applications/
+mkdir /usr/share/discord
+cat > /usr/share/discord/Discord <<EOF
+#!/bin/bash
+exec /usr/lib/Discord/Discord    --enable-features=UseOzonePlatform --ozone-platform=wayland --no-sandbox --enable-features=WaylandWindowDecorations "${@}"
+EOF
