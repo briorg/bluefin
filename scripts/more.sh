5f192dce2612f8af511c32a48d510554dd25a20c
bri <284789+b-@users.noreply.github.com>
2023-10-14 23:31:32 -0400

    feat: add discord

--- scripts/more.sh
+++ scripts/more.sh
@@ -2,3 +2,8 @@
 
 wget 'https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage' -Lo /usr/bin/nvim.appimage
 #wget 'https://download.beeper.com/linux/appImage/x64' -Lo /usr/bin/beeper.appimage
+#
+
+## install discord
+curl -L 'https://discord.com/api/download?platform=linux&format=tar.gz' | tar -xzvC /usr/lib
+cp /usr/lib/Discord/discord.desktop /usr/share/applications/
