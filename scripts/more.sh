deb9446be9e571c9fea8f9aff26ea454b0e6f811
bri <284789+b-@users.noreply.github.com>
2023-10-16 13:56:43 -0400

    fix: chmod 755 /u/sh/di/Discord

--- scripts/more.sh
+++ scripts/more.sh
@@ -8,7 +8,8 @@ wget 'https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage' 
 curl -L 'https://discord.com/api/download?platform=linux&format=tar.gz' | tar -xzvC /usr/lib
 cp /usr/lib/Discord/discord.desktop /usr/share/applications/
 mkdir /usr/share/discord
-cat > /usr/share/discord/Discord <<EOF
+cat > /usr/share/discord/Discord <<'EOF'
 #!/bin/bash
 exec /usr/lib/Discord/Discord    --enable-features=UseOzonePlatform --ozone-platform=wayland --no-sandbox --enable-features=WaylandWindowDecorations "${@}"
 EOF
+chmod 755 /usr/share/discord/Discord
