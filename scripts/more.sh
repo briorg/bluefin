36dded9704e31537e5064ef7ab7f565672427f26
bri <284789+b-@users.noreply.github.com>
2023-11-27 19:19:36 -0500

    Rebase

--- scripts/more.sh
+++ scripts/more.sh
@@ -2,22 +2,3 @@
 
 wget 'https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage' -Lo /usr/bin/nvim.appimage
 #wget 'https://download.beeper.com/linux/appImage/x64' -Lo /usr/bin/beeper.appimage
-
-# # Dynamically create the required groups via sysusers.d
-# # and set the GID based on the files we just chgrp'd
-# tail /usr/lib/sysusers.d/onepassword.conf <<EOF
-# g onepassword ${GID_ONEPASSWORD}
-# EOF
-# tail /usr/lib/sysusers.d/onepassword-cli.conf <<EOF
-# g onepassword-cli ${GID_ONEPASSWORDCLI}
-# EOF
-
-
-tail /usr/lib/tmpfiles.d/onepassword.conf <<'EOF'
-#L  /opt/1Password                           -     -  -            -  /usr/lib/1Password
-C+  /run/1Password                           -     -  -            -  /usr/lib/1Password
-L   /opt/1Password                           -     -  -            -  /run/1Password
-z   /run/1Password/1Password-BrowserSupport  2755  -  onepassword  -  -
-z   /run/1Password/chrome-sandbox            4755  -  onepassword  -  -
-EOF
-
